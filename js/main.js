1708d2924e1a551a0bd7c496d4496b6941fd4b5a
shuii18 <paullibre18@gmail.com>
2022-10-30 13:22:52 +0100

    Añadida funcionalidad responsive a la página para pantallas medianas y pequeñas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
+// VARIABLES DECLARE
+
 let tvScreen = document.getElementById("tv");
 let psOnButton = document.getElementById("onLight");
 let psEjectButton = document.getElementById("ejectLight");
+
+// LOOP SOUNDS
+
+document.getElementById("startupSound").addEventListener(
+  "ended",
+  function () {
+    this.currentTime = 0;
+    this.play();
+  },
+  false
+);
+document.getElementById("staticSound").addEventListener(
+  "ended",
+  function () {
+    this.currentTime = 0;
+    this.play();
+  },
+  false
+);
 
 // OPEN TV FUNCTION
 function tvOn() {
   if (psOnButton.classList.contains("light-power") == true) {
     tvScreen.classList.add("screen-on-display");
+    document.getElementById("startupSound").volume = 0.5;
     document.getElementById("startupSound").play();
   } else {
     tvScreen.classList.add("screen-on-noDisplay");
@@ -19,12 +41,14 @@ function tvOff() {
   document.getElementById("tv").classList.remove("screen-on-display");
   document.getElementById("tv").classList.remove("screen-on-noDisplay");
   document.getElementById("staticSound").volume = 0;
+  document.getElementById("startupSound").volume = 0;
 }
 
 // OPEN/CLOSE PS2
 function openPs2() {
   if (psOnButton.classList.contains("light-power")) {
     psOnButton.classList.remove("light-power");
+    document.getElementById("startupSound").volume = 0;
     if (tvScreen.classList.contains("screen-on-display")) {
       tvScreen.classList.remove("screen-on-display");
       tvScreen.classList.add("screen-on-noDisplay");
@@ -38,6 +62,8 @@ function openPs2() {
       tvScreen.classList.remove("screen-on-noDisplay");
       document.getElementById("staticSound").volume = 0;
       tvScreen.classList.add("screen-on-display");
+      document.getElementById("startupSound").volume = 0.5;
+      document.getElementById("startupSound").play();
     }
   }
 }
